a1fa8cb78827949e81709168232e23ac21fe4129
91903 <91903@DESKTOP-TNMOMON>
2026-04-24 14:36:29 +0530

    my first commit basic files

---
+++ src/main/java/com/project/controller/TestRestController.java
@@ -0,0 +1,32 @@
+package com.project.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/MyRestProject")
+public class TestRestController {
+	
+	@GetMapping("/getInvitation/{myJob}/{myCity}")
+	public ResponseEntity<String> getInvitation(@RequestParam("myName") String name, @RequestParam("myId") int id, 
+			@PathVariable String myJob, @PathVariable("myCity") String city) {
+		
+		//request url= http://localhost:8080/MyRestProject/getInvitation/Java Development with kafka/Bangalore?myName=Tejashwini Tiraki&myId=103
+		
+		String msg = "Hello!! your name is " + name 
+				+ " and your ID is " + id 
+				+ " you are invited to my first Rest app!! "
+				+ " and your job here is  " + myJob
+				+ " your base location is " + city;
+		
+		return new ResponseEntity<String>(msg,HttpStatus.OK);
+		
+	}
+	
+
+}
