46660fd5138b6d616990dc7a4df08f367f4d954b
Tejashwini-developer <tejashwinitiraki@gmail.com>
2026-06-04 19:49:08 +0530

    Update PracticeProjectApplication.java

--- src/main/java/com/project/PracticeProjectApplication.java
+++ src/main/java/com/project/PracticeProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PracticeProjectApplication {
 	public static void main(String[] args) {
+		System.out.println("Hello Teju Its from main class app");
 		SpringApplication.run(PracticeProjectApplication.class,args);
 		
 	}
